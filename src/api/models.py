0863cbf0227a3ee8383344e87544caf1ce1e5f05
Mariona Estrada <165200714+estradapm@users.noreply.github.com>
2024-07-19 11:50:57 +0000

    guardar

--- src/api/models.py
+++ src/api/models.py
@@ -15,6 +15,7 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active
             # no serializar la contraseña por razones de seguridad
         }
 
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Character, Planet, Vehicle, Favourite
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -12,11 +12,67 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
+@api.route('/get_user', methods=['GET'])
+def get_user():
+    users = User.query.all()
+    results = []
+    for user in users:
+       results.append(user.serialize())
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        'message': 'Ok',
+        'results':results
+    }
+
+    return jsonify(response_body), 200
+
+
+@api.route('/post_user', methods=['POST'])
+def post_user():
+    request_body = request.get_json()
+    user =  User(email = request_body['email'],
+                 password = request_body['password'],
+                 is_active = request_body['is_active'])
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(request_body), 200
+
+
+@api.route('/edit_user/<int:id>', methods=['PUT'])
+def edit_user(id):
+    request_body = request.get_json()
+    user = User.query.get(id)
+    if user is None:
+        raise APIException('User not found', status_code=404)
+    if "email" in request_body:
+        user.email = request_body["email"]
+    if "password" in request_body:
+        user.password = request_body ["password"]
+    if "is_active" in request_body:
+        user.is_active = request_body ["is_active"]
+
+    db.session.commit()
+    return jsonify(user.serialize()), 200
+
+@api.route('/delete_user/<int:id>', methods=['DELETE'])
+def delete_user (id):
+    user = User.query.get(id)
+    if user is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify("Ok"), 200
+
+
+@api.route('/get_character', methods=['GET'])
+def get_character():
+    characters = Character.query.all()
+    results = []
+    for character in characters:
+        results.append(character.serialize())
+    response_body = {
+        'message': 'Ok',
+        'results': results
     }
 
     return jsonify(response_body), 200
